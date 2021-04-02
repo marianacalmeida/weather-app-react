8baf0f13480b88082387667c9e357298a9db7a3d
marianacalmeida <71830502+marianacalmeida@users.noreply.github.com>
2021-04-02 12:07:06 +0100

    corrected className

--- src/Form.js
+++ src/Form.js
@@ -6,7 +6,7 @@ export default function Form() {
   return (
     <form id="searchForm">
       <div className="form-row">
-        <div class="form-group col-5">
+        <div className="form-group col-5">
           <input
             type="search"
             className="form-control search"
